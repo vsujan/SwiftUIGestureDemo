529fc7801be05f3f1904cbc5c0015b7715ebcf09
Sujan Vaidya <shujan1992@gmail.com>
2020-01-03 14:39:44 +0545

    combining multiple gesture

--- SwiftUIGestureDemo/SwiftUIGestureDemo/ContentView.swift
+++ SwiftUIGestureDemo/SwiftUIGestureDemo/ContentView.swift
@@ -9,23 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    // For long press gesture
+    @GestureState private var isPressed = false
+    
+    // For drag gesture
     @GestureState private var dragOffset = CGSize.zero
     @State private var position = CGSize.zero
     
     var body: some View {
         Image(systemName: "star.circle.fill")
             .font(.system(size: 100))
+            .opacity(isPressed ? 0.5 : 1.0)
             .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
             .animation(.easeInOut)
             .foregroundColor(.green)
             .gesture(
-                DragGesture()
+                LongPressGesture(minimumDuration: 1.0)
+                    .updating($isPressed, body: { (currentState, state, transaction) in
+                        state = currentState
+                    })
+                    .sequenced(before: DragGesture())
                     .updating($dragOffset, body: { (value, state, transaction) in
-                        state = value.translation
+                        
+                        switch value {
+                        case .first(true):
+                            print("Tapping")
+                        case .second(true, let drag):
+                            state = drag?.translation ?? .zero
+                        default:
+                            break
+                        }
+                        
                     })
                     .onEnded({ (value) in
-                        self.position.height += value.translation.height
-                        self.position.width += value.translation.width
+                        
+                        guard case .second(true, let drag?) = value else {
+                            return
+                        }
+                        
+                        self.position.height += drag.translation.height
+                        self.position.width += drag.translation.width
                     })
         )
     }

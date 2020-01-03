f722b0eb2045bb1d9dae1710bd110296e00c548b
Sujan Vaidya <shujan1992@gmail.com>
2020-01-03 13:39:45 +0545

    Place the image to the dragged position

--- SwiftUIGestureDemo/SwiftUIGestureDemo/ContentView.swift
+++ SwiftUIGestureDemo/SwiftUIGestureDemo/ContentView.swift
@@ -10,17 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     @GestureState private var dragOffset = CGSize.zero
+    @State private var position = CGSize.zero
     
     var body: some View {
         Image(systemName: "star.circle.fill")
             .font(.system(size: 100))
-            .offset(x: dragOffset.width, y: dragOffset.height)
+            .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
             .animation(.easeInOut)
             .foregroundColor(.green)
             .gesture(
                 DragGesture()
                     .updating($dragOffset, body: { (value, state, transaction) in
                         state = value.translation
+                    })
+                    .onEnded({ (value) in
+                        self.position.height += value.translation.height
+                        self.position.width += value.translation.width
                     })
         )
     }
